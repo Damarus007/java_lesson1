c59f7cd78a8deef119e19d806993402fd1b3d887
Damarus007 <81462299+Damarus007@users.noreply.github.com>
2021-03-27 11:48:42 +0300

    Update Main.java

--- lesson1/src/pizza/Main.java
+++ lesson1/src/pizza/Main.java
@@ -1,7 +1,19 @@
 package pizza;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Я хочу пиццы");
+    public static void main(String[] args) throws InterruptedException {
+        String yes = "Да", no = "Нет";
+        System.out.println("Хочешь ли ты пиццу?");
+        Scanner read = new Scanner(System.in);
+        String answer = read.nextLine();
+        if(answer.equals(yes)) {
+            System.out.println("Готовлю пиццу");
+            Thread.sleep(5000);
+            System.out.println("Пицца приготовлена, кушац подано");
+        } else if(answer.equals(no)) {
+            System.out.println("Пока!");
+        }
     }
 }
